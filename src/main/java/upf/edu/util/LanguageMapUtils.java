1ffa5f483e8340a2c238c296ebbe6e63b5cc89c2
Luca <tukaaa@gmail.com>
2019-03-05 12:43:50 +0100

    Modificacion chula

--- src/main/java/upf/edu/util/LanguageMapUtils.java
+++ src/main/java/upf/edu/util/LanguageMapUtils.java
@@ -6,6 +6,7 @@ import org.apache.spark.api.java.JavaRDD;
 public class LanguageMapUtils {
 
     public static JavaPairRDD<String, String> buildLanguageMap(JavaRDD<String> lines) {
+        System.out.println("Ya esta!");
         return null;// IMPLEMENT ME
     }
 }
